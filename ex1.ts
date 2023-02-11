98482a532de2b06b35ccb764e89512e8e4168c52
johny <jskjw157@gmail.com>
2023-02-12 00:03:03 +0900

    overloading

--- ex1.ts
+++ ex1.ts
@@ -47,6 +47,7 @@ if(typeof a === 'number'){
 if(typeof a === "string"){
     a.toUpperCase();
 }
+
 //void
 function hellp(){
     console.log('x')
@@ -56,3 +57,68 @@ function hellp(){
 function hello2(): never{
     throw new Error("xxx")
 }
+
+//call signature
+type Add = (a:number, b:number) => number;
+
+const add: Add  = (a,b)=> a+b;
+
+
+//overloading
+type Add2 = {
+    (a:number, b:number) : number
+    (a:number, b:string) : number
+}
+const add2: Add2  = (a,b) => {
+    if(typeof b === "string") return a
+    return a+b
+}
+
+const last = (arr) => {
+    const length = arr.length;
+    return arr[length -1];
+}
+
+
+// overloading 실제 사례 1
+type Config = {
+    path: string,
+    state: object
+}
+
+type Push = {
+    (path: string): void
+    (config: Config): void
+}
+
+const push: Push = (config) => {
+    if(typeof config === 'string'){
+        console.log(config);
+    } 
+    else{
+        console.log(config.path);
+    }
+}
+
+// overloading 실제 사례 2
+type Add3 = {
+    (a:number, b:number) :number,
+    (a:number, b:number, c:number) :number
+}
+
+const add3:Add3 = (a, b, c?: number) => {
+    if(c) return a+b+c
+    return a + b
+}
+
+type SuperPrint = {
+    <TypePlaceholder>(arr: TypePlaceholder[]): void
+}
+
+const superPrint: SuperPrint = (arr) => {
+    arr.forEach(i => console.log(i))
+}
+
+superPrint([1, 2, 3, 4])
+superPrint([true, false, true])
+superPrint(["a","b","c"])
